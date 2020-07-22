8c0cfec6b819f9cb762105e2fabf2553a8adae26
Juan Carlos Garcia <jcgseco@gmail.com>
2020-07-23 00:20:19 +0200

    Added save token logic 🗃

--- app/src/main/java/com/ikarimeister/loginapp/domain/usecases/Login.kt
+++ app/src/main/java/com/ikarimeister/loginapp/domain/usecases/Login.kt
@@ -1,17 +1,26 @@
 package com.ikarimeister.loginapp.domain.usecases
 
 import arrow.core.Either
+import arrow.core.extensions.fx
 import com.ikarimeister.loginapp.data.LoginApiClient
+import com.ikarimeister.loginapp.data.TokenRepository
 import com.ikarimeister.loginapp.data.network.FakeLoginApiClient
 import com.ikarimeister.loginapp.domain.model.LoginError
 import com.ikarimeister.loginapp.domain.model.Token
 import com.ikarimeister.loginapp.domain.model.User
 
-class Login(private val apiClient: LoginApiClient = Dependencies.apiClient) {
-    operator fun invoke(user: User): Either<LoginError, Token> =
-            apiClient.login(user)
+class Login(
+    private val apiClient: LoginApiClient = Companion.apiClient,
+    private val repository: TokenRepository = Companion.repository
+) {
+    operator fun invoke(user: User): Either<LoginError, Token> = Either.fx {
+        val token = apiClient.login(user).bind()
+        repository + token
+        token
+    }
 
-    companion object Dependencies {
-        val apiClient = FakeLoginApiClient
+    companion object {
+        val apiClient by lazy { FakeLoginApiClient }
+        val repository by lazy { TokenRepository() }
     }
 }
