120fbe7d1c91ab9469358561538aa97fb225eadd
Juan Carlos Garcia <jcgseco@gmail.com>
2020-07-28 21:22:55 +0200

    Profile repository and preferences data source added 🛠

--- app/src/main/java/com/ikarimeister/loginapp/di.kt
+++ app/src/main/java/com/ikarimeister/loginapp/di.kt
@@ -2,12 +2,18 @@ package com.ikarimeister.loginapp
 
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
+import com.google.gson.Gson
 import com.ikarimeister.loginapp.data.ConfigurationRepository
 import com.ikarimeister.loginapp.data.LoginApiClient
 import com.ikarimeister.loginapp.data.local.ConfigurationDataSource
+import com.ikarimeister.loginapp.data.local.JsonSerializer
+import com.ikarimeister.loginapp.data.local.serializers.ProfileGsonSerializer
+import com.ikarimeister.loginapp.data.local.sharedpreferences.SharedPreferencesProfileDataSource
 import com.ikarimeister.loginapp.data.local.sharedpreferences.SharedPreferencesTokenDataSource
 import com.ikarimeister.loginapp.data.network.FakeLoginApiClient
+import com.ikarimeister.loginapp.data.repositories.ProfileRepository
 import com.ikarimeister.loginapp.data.repositories.TokenRepository
+import com.ikarimeister.loginapp.domain.model.Profile
 import com.ikarimeister.loginapp.domain.model.Token
 import com.ikarimeister.loginapp.domain.usecases.IsLoginStored
 import com.ikarimeister.loginapp.domain.usecases.Login
@@ -38,30 +44,51 @@ fun Application.initDI() {
 val appModule = module {
     single<CoroutineDispatcher>(named("UI")) { Dispatchers.Main }
     single<CoroutineDispatcher>(named("BG")) { Dispatchers.IO }
+    single {
+        androidApplication()
+                .getSharedPreferences(LoginApp.sharedName, MODE_PRIVATE)
+    }
+    single { Gson() }
 }
 
 val dataModule = module {
-    single { androidApplication()
-            .getSharedPreferences(LoginApp.sharedName, MODE_PRIVATE) }
-    single<ConfigurationDataSource<Token>> {
-        SharedPreferencesTokenDataSource(get())
+    single<JsonSerializer<Profile>>(named("Profile")) { ProfileGsonSerializer(gson = get()) }
+    single<ConfigurationDataSource<Token>>(named("Token")) {
+        SharedPreferencesTokenDataSource(preferences = get())
     }
-    single<ConfigurationRepository<Token>> { TokenRepository(get()) }
+    single<ConfigurationRepository<Token>>(named("Token")) {
+        TokenRepository(datasource = get(named("Token")))
+    }
+    single<ConfigurationDataSource<Profile>>(named("Profile")) {
+        SharedPreferencesProfileDataSource(preferences = get(), jsonSerializer = get(named("Profile")))
+    }
+    single<ConfigurationRepository<Profile>>(named("Profile")) { ProfileRepository(datasource = get(named("Profile"))) }
     factory<LoginApiClient> { FakeLoginApiClient }
 }
 
 val scopesModule = module {
     scope(named<LoginActivity>()) {
         factory { (view: LoginView) ->
-            LoginPresenter(view, get(), get(), get(named("BG")), get(named("UI")))
+            LoginPresenter(
+                    view = view,
+                    login = get(),
+                    isLoginStored = get(),
+                    bgDispatcher = get(named("BG")),
+                    uiDispacher = get(named("UI"))
+            )
         }
-        scoped { Login(get(), get()) }
-        scoped { IsLoginStored(get()) }
+        scoped { Login(repository = get(named("Token")), apiClient = get()) }
+        scoped { IsLoginStored(repository = get(named("Token"))) }
     }
     scope(named<MainActivity>()) {
         factory { (view: MainView) ->
-            MainPresenter(view, get(), get(named("BG")), get(named("UI")))
+            MainPresenter(
+                    view = view,
+                    logout = get(),
+                    bgDispatcher = get(named("BG")),
+                    uiDispacher = get(named("UI"))
+            )
         }
-        scoped { Logout(get()) }
+        scoped { Logout(repository = get(named("Token"))) }
     }
 }
